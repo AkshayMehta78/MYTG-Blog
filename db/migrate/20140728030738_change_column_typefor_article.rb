10a5f73a94053c5915dfefc613e35449434383da
akshaymehta78 <akshay.mehta@vit.edu.in>
2014-07-27 20:36:17 -0700

    Deployment

--- db/migrate/20140728030738_change_column_typefor_article.rb
+++ db/migrate/20140728030738_change_column_typefor_article.rb
@@ -1,5 +1,5 @@
 class ChangeColumnTypeforArticle < ActiveRecord::Migration
   def change
-change_column :Articles, :body, :text, :limit => nil
+change_column :Articles, :body, :text
   end
 end
---
+++ db/migrate/20140728032703_change_column_type.rb
@@ -0,0 +1,5 @@
+class ChangeColumnType < ActiveRecord::Migration
+ def change
+  change_column :Articles, :body, :text, :limit => nil
+end
+end
